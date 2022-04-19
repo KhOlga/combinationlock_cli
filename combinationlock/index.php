c85ea4ccb2a177f1c7cc5c446652702e62cf289a
Olha Kharabet <kharabet.olga@gmail.com>
2022-04-19 22:15:25 +0200

    Added logic to get possible random lock's options

--- combinationlock/index.php
+++ combinationlock/index.php
@@ -3,32 +3,48 @@
 $ringAmount = $argv[1];
 settype($ringAmount, "integer");
 
-if ($ringAmount >= 4) {
-	$firstPart = [];
-	$firstRingKey = rand(0, 4);
-	$firstRingValues = array(1, 3, 5, 7, 9);
-	$firstPart[] = $firstRingValues[$firstRingKey];
+$firstRingValues = array(1, 3, 5, 7, 9);
+$values = array(0, 1, 2, 3, 5, 6, 7, 8, 9);
 
-	$secondPart = [];
-	$key = rand(0, 8);
-	$values = array(0, 1, 2, 3, 5, 6, 7, 8, 9);
-	$size = $ringAmount - 3;
+$optionsAmount = count($firstRingValues);
 
-	for ($i = 0; $i < $size; $i++) {
-		array_push($secondPart, $values[rand(0, 8)]);
+for ($i = 0; $i < $ringAmount; $i++) {
+	$optionsAmount *= count($values);
+}
+echo $optionsAmount . "\n";
+$counter = 1;
+$lockOptions = [];
+while ($counter <= $optionsAmount) {
+
+	if ($ringAmount >= 4) {
+		$firstPart = [];
+		$firstRingKey = rand(0, 4);
+		$firstPart[] = $firstRingValues[$firstRingKey];
+
+		$secondPart = [];
+		$key = rand(0, 8);
+		$size = $ringAmount - 3;
+
+		for ($i = 0; $i < $size; $i++) {
+			array_push($secondPart, $values[$key]);
+		}
+
+		$secondPart[] = 5;
+		$secondPart[] = 6;
+		sort($secondPart);
+
+		$merged = array_merge($firstPart, $secondPart);
+
+		$lock = null;
+		foreach ($merged as $number) {
+			$lock .= $number;
+		}
+		echo "Option #$counter: $lock\n";
+		$lockOptions[] = $lock;
+
+		$counter++;
+
+	} else {
+		echo "An amount of rings in the lock can not be less than 4.\n";
 	}
-
-	$secondPart[] = 5;
-	$secondPart[] = 6;
-	sort($secondPart);
-
-	$merged = array_merge($firstPart, $secondPart);
-
-	$lock = null;
-	foreach ($merged as $number) {
-		$lock .= $number;
-	}
-	echo $lock . "\n";
-} else {
-	echo "An amount of rings in the lock can not be less than 4.\n";
 }
